b6bf799b95b4374c64b54b3a8e6f0e0dcf2a17b6
Anthony <shima.anthony@gmail.com>
2024-08-23 17:22:51 -0700

    Update reviews.router.js

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -2,11 +2,10 @@ const router = require("express").Router();
 const controller = require("./reviews.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-
 router
-    .route("/:reviewId")
-    .delete(controller.delete)
-    .put(controller.update)
-    .all(methodNotAllowed)
+  .route("/:reviewId")
+  .put(controller.update)
+  .delete(controller.delete)
+  .all(methodNotAllowed);
 
-module.exports = router
+module.exports = router;
