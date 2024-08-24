98cf34fe95574c446fc3172eebc6dc1da0bc289e
Anthony <shima.anthony@gmail.com>
2024-08-23 17:23:04 -0700

    Update reviews.service.js

--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -1,44 +1,45 @@
-const knex = require("../db/connection")
+const knex = require("../db/connection");
+const mapProperties = require("../utils/map-properties");
 
-function read(reviewId){
-    return knex("reviews")
-    .select("*")
-    .where({ "review_id":reviewId })
+const addCritic = mapProperties({
+  c_critic_id: "critic.critic_id",
+  preferred_name: "critic.preferred_name",
+  surname: "critic.surname",
+  organization_name: "critic.organization_name",
+  c_created_at: "critic.created_at",
+  c_updated_at: "critic.updated_at",
+});
+
+const read = review_id => {
+  return knex("reviews as r")
+    .join("critics as c", "c.critic_id", "r.critic_id")
+    .select(
+      "r.*",
+      "c.critic_id as c_critic_id",
+      "c.preferred_name",
+      "c.surname",
+      "c.organization_name",
+      "c.created_at as c_created_at",
+      "c.updated_at as c_updated_at"
+    )
+    .where({ review_id })
     .first()
-}
+    .then(addCritic);
+};
 
-// function getReviewsWithCritic(reviewId){
-//     return knex("reviews as r")
-//     .join("critics as c", "r.critic_id", "c.critic_id" )
-//     .select("r.*")
-//     .where({ "r.review_id":reviewId })
-// }
-
-function getCritic(criticId){
-    return knex("critics")
+const update = updatedReview => {
+  return knex("reviews")
     .select("*")
-    .where({"critic_id":criticId})
-}
+    .where({ review_id: updatedReview.review_id })
+    .update(updatedReview, "*");
+};
 
-function destroy(reviewId){
-
-    return knex("reviews")
-    .where({ "review_id":reviewId })
-    .del()
-}
-
-function update(updatedReview, id){
-    return knex("reviews")
-    .select("*")
-    .where({ review_id: id })
-    .update({...updatedReview} )
-    //.then((updatedRecords) =>updatedRecords[0])
-}
+const destroy = review_id => {
+  return knex("reviews").where({ review_id }).del();
+};
 
 module.exports = {
-    read,
-    delete: destroy,
-    getCritic,
-    update,
-
-}
+  read,
+  update,
+  delete: destroy,
+};
