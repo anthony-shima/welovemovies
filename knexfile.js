7496a9255ae7c1bf0ab04f0f95a69e1c145aa939
Anthony <shima.anthony@gmail.com>
2024-08-24 10:33:38 -0700

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgres://ylnzgbhp:eE4CrR-GK4yoIkNvmyXSgHwEAYA82mAz@heffalump.db.elephantsql.com/ylnzgbhp",
+  DATABASE_URL,
 } = process.env;
 
 module.exports = {
